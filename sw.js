c275108660de0256e6832b89006d02cd84f19fd2
elmokhtarmchich <79937072+elmokhtarmchich@users.noreply.github.com>
2023-10-29 14:10:06 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('radio-store').then((cache) => cache.addAll([
+      '/index.html',
+      '/index.js',
+      '/style.css',
+      '/sw.js',
+      '/script.js',
+	    '/audioPlayer.js',
+      './image/med.png',
+      './image/coran.png',
+    ])),
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
+});
+
